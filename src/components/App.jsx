b991d1ce8b88756da6d8ea51abc19523b26a0ce5
Michał Murawski <m.a.murawski@wp.pl>
2023-01-16 14:20:31 +0100

    change file name

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 import { Profile } from './Profile/Profile';
-import user from '../json/user.json';
+import user from '../data/user.json';
 import { Statistics } from './Statistics/Statistics';
-import statistics from '../json/statistics.json';
+import statistics from '../data/statistics.json';
 import { FriendList } from './FriendList/FriendList';
-import friends from '../json/friends.json';
+import friends from '../data/friends.json';
 import { TransactionHistory } from './TransactionHistory/TransactionHistory';
-import transactions from '../json/transactions.json';
+import transactions from '../data/transactions.json';
 
 export const App = () => {
   return (
